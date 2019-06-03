120e79af0b29de9f3f9a3b07ef75f332d49859a3
communal-tutorial-3267 <jrodden1@gmail.com>
2019-06-03 05:08:08 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -39,6 +39,8 @@ end
 
 def word_substituter(tweet)
   finishedTweet = ""
+  #I want to split the string into an array using spaces, single quotes, and commas
+  #used this as a reference https://stackoverflow.com/questions/19509307/split-string-by-multiple-delimiters
   tweetArr = tweet.split /\s|'|,/
   tweetArr.map do |word|
     #is the word in the dictionary?  I'm testing for the blank output of the #dictionary method which means the word is not in the dictionary and nothing can be done to it.
